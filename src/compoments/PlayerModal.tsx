b619a0940063865fb0241d587a377e5a8abd53c8
IgnaSebas <sebastian.igna.kb@gmail.com>
2025-03-16 16:25:01 +0100

    Upgrade PlayerModal and PlayerIcon styling

--- src/compoments/PlayerModal.tsx
+++ src/compoments/PlayerModal.tsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { Player } from "../types/Player";
+import "./PlayerModal.css";
 
 interface PlayerModalProps {
     onClose: () => void;
@@ -33,29 +34,31 @@ function PlayerModal ({ onClose, addPlayer, playersList}: PlayerModalProps) {
 
     console.log("Players List: ", playersList)
     return (
-        <div className="modal" onClick={(e) => e.stopPropagation()}>
-            <div className="modal-header">
-                <div style={{ width:"45px", height: "45px" }}></div>
-                <h2>Select Player</h2>
-                <button style={{ width:"45px", height: "45px" }} onClick={handleClose}>X</button>
-            </div>
-            <div className="modal-body">
-                <div className="input-container">
-                    <input placeholder="Search..." value={searchTerm} onChange={(e) => handlePlayerSearch(e.target.value)} />
+        <div className="modal-overlay" onClick={onClose}>
+            <div className="modal" onClick={(e) => e.stopPropagation()}>
+                <div className="modal-header">
+                    <div style={{ width:"45px", height: "45px" }}></div>
+                    <h2>Select Player</h2>
+                    <button style={{ width:"45px", height: "45px" }} onClick={handleClose}>X</button>
                 </div>
-                <div className="grid-container-players">
-                    {filteredList.map((player, index) => (
-                        <div key={index} style={{ display:"flex", flexDirection:"column", alignItems:"center", cursor:"pointer",  backgroundColor: `${choosenPlayer === player ? "yellow" : ""}`}} onClick={() => setChoosenPlayer(player)}>
-                            <img src={player.image} alt={player.name} style={{ maxWidth:"100px", maxHeight:"100px" }} />
-                            <div className="empty-player-icon">
-                                <span>{player.name}</span>
+                <div className="modal-body">
+                    <div className="input-container">
+                        <input placeholder="Search..." value={searchTerm} onChange={(e) => handlePlayerSearch(e.target.value)} />
+                    </div>
+                    <div className="grid-container-players">
+                        {filteredList.map((player, index) => (
+                            <div key={index} style={{ display:"flex", flexDirection:"column", alignItems:"center", cursor:"pointer",  backgroundColor: `${choosenPlayer === player ? "yellow" : ""}`}} onClick={() => setChoosenPlayer(player)}>
+                                <img src={player.image} alt={player.name} style={{ maxWidth:"100px", maxHeight:"100px" }} />
+                                <div className="empty-player-icon">
+                                    <span>{player.name}</span>
+                                </div>
                             </div>
-                        </div>
-                    ))}
+                        ))}
+                    </div>
                 </div>
-            </div>
-            <div className="modal-footer">
-                <a className={`secondary-button ${choosenPlayer ? "" : "disabled"}`} onClick={() => choosenPlayer && handleAddPlayer(choosenPlayer)}>Add Player</a>
+                <div className="modal-footer">
+                    <a className={`secondary-button ${choosenPlayer ? "" : "disabled"}`} onClick={() => choosenPlayer && handleAddPlayer(choosenPlayer)}>Add Player</a>
+                </div>
             </div>
         </div>
     );
